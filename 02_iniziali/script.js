b3ac40925e4ca81be2791405fa5a292664d0128e
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-24 13:32:05 +0100

    Add logic to createNewArray function to extract first letters from list

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -6,6 +6,11 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 // Dichiara la funzione qui.
 function createNewArray(list){
     const newArray = [];
+    for(let i = 0; i < list.length; i++){
+        const element = list[i];
+        const firstLetter = element.charAt(0);
+        newArray.push(firstLetter);
+    }
 }
 // Invoca la funzione qui e stampa il risultato in console
 
